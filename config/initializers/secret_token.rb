633a4c439604172e99e3901fde0b11512f714265
Arthur Toper <arthur@unomee.com>
2012-07-21 14:09:09 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nathanphotos::Application.config.secret_token = 'cbad3784111d2b1e45a58dc2933ae852a914ad1dfa897a5219bb1053a7dbd8503e3d38720b72ca051947ac3b759283a7ec503d9dee8ebcb6ff0e66d572e2b336'
