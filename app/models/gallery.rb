bf5a4779ee9e5b832f8cbeda4cab0b3920544d73
Arthur Toper <arthur@unomee.com>
2012-07-21 16:20:24 +0100

    Tarted up the application layout somewhat.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,7 +1,11 @@
 class Gallery < ActiveRecord::Base
-  attr_accessible :description, :name
+	attr_accessible :description, :name
 
-  validates :name, presence: true
+	validates :name, presence: true
 
-  has_many :photographs, dependent: :destroy
+	has_many :photographs, dependent: :destroy
+
+	def image
+		photographs.last if photographs.any? && photographs.last.image?
+	end
 end
