bc60ac81d0cca57e58b0f16ff59759bd6c31fca1
Arthur Toper <arthur@unomee.com>
2012-07-29 15:32:15 +0100

    Added ability to create new services and view them.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,4 +2,21 @@ class ServicesController < ApplicationController
 	def index
 		@services = Service.all
 	end
+
+	def new
+		@service = Service.new
+	end
+
+	def create
+		@service = Service.new params[:service]
+		if @service.save
+			redirect_to services_url
+		else
+			render :new
+		end
+	end
+
+	def show
+		@service = Service.find params[:id]
+	end
 end
