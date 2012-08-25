549b7f057ee4d3195a8a0adff1370135279a5b38
Arthur Toper <arthur@unomee.com>
2012-08-25 16:41:24 +0100

    Tweaked column widths on gallery pages.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,11 +8,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 	end
 
 	version :thumb do
-		process :resize_to_limit => [200, 200]
+		process :resize_to_limit => [59, 59]
 	end
 
 	version :large do
-		process :resize_to_limit => [763, 500]
+		process :resize_to_limit => [937, 700]
 	end
 
 	def extension_white_list
